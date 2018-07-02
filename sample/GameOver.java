e85ad6a67c24cd3c8047db38dc644ff2ffe55a6d
TheCakegamer <40315936+TheCakegamer@users.noreply.github.com>
2018-07-02 15:47:03 +0200

    Game Over Sound

--- sample/GameOver.java
+++ sample/GameOver.java
@@ -3,6 +3,8 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 
@@ -13,7 +15,9 @@ public class GameOver {
     Main main = new Main();
 
     public void closegame(ActionEvent actionEvent) throws Exception {
-
+        Media oof = new Media(getClass().getResource("/image/oof.mp3").toExternalForm());
+        MediaPlayer oofplayer = new MediaPlayer(oof);
+        oofplayer.play();
         Stage GOwindow = (Stage) GObutton.getScene().getWindow();
         GOwindow.close();
         main.start(main.primaryStage);
