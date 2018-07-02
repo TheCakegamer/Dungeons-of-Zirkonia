19a075779c92535db7c334853498d25b84c81bf8
TheCakegamer <40315936+TheCakegamer@users.noreply.github.com>
2018-07-02 16:11:55 +0200

    Display Name

--- sample/Quit.java
+++ sample/Quit.java
@@ -2,13 +2,23 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 
 
 public class Quit {
 
     @FXML
     Button quitbutton;
+    @FXML
+    Label text;
+
+    public void initialize(){
+        text.setText("Do you really want to quit, " + System.getProperty("user.name") + "?");
+
+    }
 
     public void quit(){
         System.exit(1);
